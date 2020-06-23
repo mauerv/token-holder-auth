f04a4ca41922381c5b9a776b087e7eb1cb990709
Mauro <mauro@osn.global>
2020-06-23 17:13:52 -0300

    Add local signature with web3

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,251 +1,36 @@
 import React from "react";
 import Web3 from "web3";
 
-const abi = [
-  {
-    constant: true,
-    inputs: [],
-    name: "name",
-    outputs: [
-      {
-        name: "",
-        type: "string",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_spender",
-        type: "address",
-      },
-      {
-        name: "_value",
-        type: "uint256",
-      },
-    ],
-    name: "approve",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "totalSupply",
-    outputs: [
-      {
-        name: "",
-        type: "uint256",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_from",
-        type: "address",
-      },
-      {
-        name: "_to",
-        type: "address",
-      },
-      {
-        name: "_value",
-        type: "uint256",
-      },
-    ],
-    name: "transferFrom",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "decimals",
-    outputs: [
-      {
-        name: "",
-        type: "uint8",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "_owner",
-        type: "address",
-      },
-    ],
-    name: "balanceOf",
-    outputs: [
-      {
-        name: "balance",
-        type: "uint256",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "symbol",
-    outputs: [
-      {
-        name: "",
-        type: "string",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_to",
-        type: "address",
-      },
-      {
-        name: "_value",
-        type: "uint256",
-      },
-    ],
-    name: "transfer",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "_owner",
-        type: "address",
-      },
-      {
-        name: "_spender",
-        type: "address",
-      },
-    ],
-    name: "allowance",
-    outputs: [
-      {
-        name: "",
-        type: "uint256",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    payable: true,
-    stateMutability: "payable",
-    type: "fallback",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        name: "owner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        name: "spender",
-        type: "address",
-      },
-      {
-        indexed: false,
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Approval",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        name: "from",
-        type: "address",
-      },
-      {
-        indexed: true,
-        name: "to",
-        type: "address",
-      },
-      {
-        indexed: false,
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Transfer",
-    type: "event",
-  },
-];
-
 function App() {
   const validateToken = async () => {
+    // await window.ethereum.enable();
+    // const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    // const accounts = await web3.eth.getAccounts();
+    // const currentAccount = accounts[0];
+    // const contractAddr = "0x7Ce5A09Bf3A1b2A2d34d63DAFb46ae41dD734Ca8";
+    // const contract = new web3.eth.Contract(abi, contractAddr);
+    // const balance = await contract.methods
+    //   .balanceOf(currentAccount)
+    //   .call({ from: currentAccount });
+    // const instance = contract.at();
+    // // Get balance of my address.
+    // const balance = await instance.balanceOf(currentAccount);
+    //console.log(balance);
+  };
+
+  const validateAddress = async () => {
     await window.ethereum.enable();
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const accounts = await web3.eth.getAccounts();
     const currentAccount = accounts[0];
-    const contractAddr = "0x7Ce5A09Bf3A1b2A2d34d63DAFb46ae41dD734Ca8";
-    const contract = new web3.eth.Contract(abi, contractAddr);
-    const balance = await contract.methods
-      .balanceOf(currentAccount)
-      .call({ from: currentAccount });
-
-    // const instance = contract.at();
-    // // Get balance of my address.
-    // const balance = await instance.balanceOf(currentAccount);
-    console.log(balance);
+    const signature = await web3.eth.personal.sign("Some text", currentAccount);
+    console.log(signature);
   };
 
   return (
     <div className="App">
       <h3>Do you have the token?</h3>
-      <button onClick={validateToken}>Validate Token</button>
+      <button onClick={validateAddress}>Validate Token</button>
     </div>
   );
 }
