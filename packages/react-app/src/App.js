90997e871e957fa176f5247dc7637b4905579074
Mauro <mauro@osn.global>
2020-06-24 14:33:25 -0300

    Add axios and send signature data to server

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Web3 from "web3";
+const axios = require("axios").default;
 
 function App() {
   const validateToken = async () => {
@@ -22,15 +23,30 @@ function App() {
     await window.ethereum.enable();
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const accounts = await web3.eth.getAccounts();
-    const currentAccount = accounts[0];
-    const signature = await web3.eth.personal.sign("Some text", currentAccount);
-    console.log(signature);
+    const owner = accounts[0];
+    const message = "Some text";
+    const messageHash = web3.utils.toHex(message);
+    const signature = await web3.eth.personal.sign(message, owner);
+
+    axios.post("http://localhost:5000/validate", {
+      owner,
+      message,
+      messageHash,
+      signature,
+    });
+  };
+
+  const checkServer = async () => {
+    axios
+      .get("http://localhost:5000/echo")
+      .then((response) => console.log(response));
   };
 
   return (
     <div className="App">
       <h3>Do you have the token?</h3>
       <button onClick={validateAddress}>Validate Token</button>
+      <button onClick={checkServer}>Check Server</button>
     </div>
   );
 }
